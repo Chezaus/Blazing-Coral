7f9f1f345fbf64b535ba2f0f36cabfbe42e083f0
Chezaus <109493757+Chezaus@users.noreply.github.com>
2024-05-17 20:25:26 -0400

    win
    -beating the boss now brings you to win screen

--- Assets/Scripts/Rounds/RoundCounter.cs
+++ Assets/Scripts/Rounds/RoundCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoundCounter : MonoBehaviour
 {
@@ -75,9 +76,7 @@ public class RoundCounter : MonoBehaviour
             break;
                 case 5: round5();
             break;
-                case 6: round2();
-            break;
-                case 7: round2();
+                case 6: SceneManager.LoadScene("win");
             break;
 
         }
